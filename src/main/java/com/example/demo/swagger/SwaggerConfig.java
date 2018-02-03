d4601399cb006e6c4ead872cc1d59d42a2e6e1c4
Ovanan <saichandmar111@gmail.com>
2018-02-03 22:18:35 +0530

    ok

--- src/main/java/com/example/demo/swagger/SwaggerConfig.java
+++ src/main/java/com/example/demo/swagger/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -19,9 +20,9 @@ public class SwaggerConfig {
 	public Docket api() {                
 		return new Docket(DocumentationType.SWAGGER_2)          
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.demo"))
-				//.paths(PathSelectors.any())
-				.paths(regex("/orders.*"))
+				.apis(RequestHandlerSelectors.basePackage("com.demo.order"))
+				.paths(PathSelectors.any())
+				//.paths(regex("/orders-service.*"))
 				.build()
 				.apiInfo(apiInfo());
 	}
